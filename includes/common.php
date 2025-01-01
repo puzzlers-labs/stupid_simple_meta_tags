7c5217320a40d3d0377b8db6a1a6f2d72be858e1
Puzzlers Labs <tech@puzzlers-labs.com>
2025-01-02 04:56:50 +0900

    Initial Commit

---
+++ includes/common.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Common functions for the plugin.
+ * @author Puzzlers Labs Pvt. Ltd. <tech@puzzlers-labs.com>
+ */
+
+function stupid_simple_meta_tags_get_version()
+{
+    return STUPID_SIMPLE_META_TAGS_VERSION;
+}
+
+function stupid_simple_meta_tags_init() {}
