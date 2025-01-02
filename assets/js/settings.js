66155c36bb6ad6d4babc257eac0c54a4c925663e
Puzzlers Labs <tech@puzzlers-labs.com>
2025-01-03 04:31:48 +0900

    Add delete function

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -15,3 +15,21 @@ const addRow = () => {
 
     inputList.appendChild(newRow);
 }
+
+const deleteRow = (element) => {
+    const row = element.closest('.meta-tags-list-row');
+    row.remove();
+    reIndexRows();
+}
+
+const reIndexRows = () => {
+    const inputList = document.querySelector('#dynamic-inputs-list');
+    const rows = inputList.children;
+    for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        row.querySelector('.order-input').name = `stupid_simple_meta_tags_basic_settings_meta_configuration_list[${i}][order]`;
+        row.querySelector('.type-select').name = `stupid_simple_meta_tags_basic_settings_meta_configuration_list[${i}][type]`;
+        row.querySelector('.key-input').name = `stupid_simple_meta_tags_basic_settings_meta_configuration_list[${i}][key]`;
+        row.querySelector('.value-input').name = `stupid_simple_meta_tags_basic_settings_meta_configuration_list[${i}][value]`;
+    }
+}
