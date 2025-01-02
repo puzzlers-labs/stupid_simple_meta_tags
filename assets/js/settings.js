63ea0e5e80edda8ad2a2f1c4047843f5abc4dda1
Puzzlers Labs <tech@puzzlers-labs.com>
2025-01-02 18:24:23 +0900

    Add table for meta tags

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -3,10 +3,30 @@ jQuery(document).ready(function ($) {
 
     $('#add-input').on('click', function () {
         const newRow = `
-            <div class="dynamic-input-row">
-                <input type="text" name="my_plugin_dynamic_inputs[]" class="regular-text" />
-                <button type="button" class="button remove-input">Remove</button>
-            </div>`;
+            <tr class="active" data-slug="akismet" data-plugin="akismet/akismet.php">
+                        <th scope="row" class="check-column">
+                            <label class="label-covers-full-cell" for="checkbox_1a03f1c70286d2ea118f953c968249d2">
+                                <span class="screen-reader-text">Select Akismet Anti-spam: Spam Protection</span>
+                            </label>
+                            <input type="checkbox" name="checked[]" value="akismet/akismet.php" id="checkbox_1a03f1c70286d2ea118f953c968249d2">
+                        </th>
+                        <td class="plugin-title column-primary">
+                            <input type="number" name="my_plugin_dynamic_inputs[]" class="regular-text" />
+                            <div class="row-actions visible">
+                                <span class="delete">
+                                    <a href="plugins.php?action=delete-selected&amp;checked%5B0%5D=hello.php&amp;plugin_status=all&amp;paged=1&amp;s&amp;_wpnonce=6ee8644c54" id="delete-hello-dolly" class="delete" aria-label="Delete Hello Dolly">
+                                        Delete
+                                    </a>
+                                </span>
+                            </div>
+                        </td>
+                        <td class="column-description desc">
+                            <input type="text" list="options" id="typeable-select" name="typeable-select" placeholder="Type or select an option" />
+                        </td>
+                        <td class="column-auto-updates">
+                            <input type="text" name="my_plugin_dynamic_inputs[]" class="regular-text" />
+                        </td>
+                    </tr>`;
         inputList.append(newRow);
     });
 
--- pages/settings/index.php
+++ pages/settings/index.php
@@ -4,10 +4,11 @@ function stupid_simple_meta_tags_settings_init()
 {
     add_filter('admin_footer_text', 'add_stupid_simple_meta_tags_footer_message');
 
-    wp_enqueue_script('settings-js', STUPID_SIMPLE_META_TAGS_PLUGIN_URL . 'assets/js/settings.js');
+    wp_enqueue_style('common-css', STUPID_SIMPLE_META_TAGS_PLUGIN_URL . 'assets/css/common.css');
 
-    wp_enqueue_style('settings-css', STUPID_SIMPLE_META_TAGS_PLUGIN_URL . 'assets/css/common.css');
+    wp_enqueue_script('settings-js', STUPID_SIMPLE_META_TAGS_PLUGIN_URL . 'assets/js/settings.js');
     wp_enqueue_style('settings-css', STUPID_SIMPLE_META_TAGS_PLUGIN_URL . 'assets/css/settings.css');
+
     echo stupid_simple_meta_tags_settings_render();
 }
 
